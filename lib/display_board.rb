9ee6d3483f5b705857241088e2a52ee5ee9395c9
Jess Jacobson <jessanova@gmail.com>
2018-10-10 20:25:49 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@
 def display_board(board)
   puts " #{board[0]} | #{board[1]} |  #{board[2]} "
   puts "-----------"
-  puts " #{x[3]} | #{x[4]} |  #{x[5]} "
+  puts " #{board[3]} | #{board[4]} |  #{board[5]} "
   puts "-----------"
-  puts " #{x[6]} | #{x[7]} |  #{x[8]} "
+  puts " #{board[6]} | #{board[7]} |  #{board[8]} "
 end
